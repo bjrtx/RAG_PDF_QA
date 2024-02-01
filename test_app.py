a0dcdec8ee2f84f2727594f5d7790a3c7b7cb6b0
Clement Argentin <clement.argentin@sii.fr>
2024-02-01 15:35:39 +0100

    Add integration test

--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest import mock
 from main import Mistral_llm, load_data, get_answer
 
 
@@ -18,3 +19,23 @@ def test_get_answer():
     response = get_answer("test question")
     print(response)
     assert isinstance(response, str)
+
+
+# Integration test
+def test_full_interaction():
+    with mock.patch('main.vectorDB') as mock_vectorDB:
+        mock_vectorDB.return_value.query.return_value = {
+            "documents": [["Simulated response"]]
+            }
+
+        with mock.patch('main.Mistral_llm') as mock_Mistral_llm:
+            mock_MistralClient = mock.Mock()
+            mock_MistralClient.chat.return_value.choices = [mock.Mock()]
+            mock_MistralClient.chat.return_value.choices[0].message.content = "Simulated response"
+            mock_Mistral_llm.return_value = (
+                "mistral-tiny",
+                mock_MistralClient
+                )
+
+            response = get_answer("Test question")
+            assert response == "Simulated response"

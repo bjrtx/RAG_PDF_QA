25fd5ab339de7a1112033be677c7d373be7d03c6
Clement Argentin <clement.argentin@sii.fr>
2024-02-13 15:48:49 +0100

    Separate history messages for each RAG option

--- pages/RAG_PDF_uploaded.py
+++ pages/RAG_PDF_uploaded.py
@@ -3,7 +3,8 @@ from utils import (
     upload_pdf,
     display_pdf,
     prepare_data_for_mistral,
-    get_answer)
+    get_answer
+    )
 
 
 st.set_page_config(layout="wide")
@@ -20,10 +21,14 @@ if uploaded_file is not None:
             uploaded_file=uploaded_file
             )
 
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
+        section_key = "RAG_PDF"
+        if f"messages_{section_key}" not in st.session_state:
+            st.session_state[f"messages_{section_key}"] = []
 
-        question_input = st.text_input("Ask a question:", key="question_input")
+        question_input = st.text_input(
+            "Ask a question:",
+            key=f"question_input_{section_key}"
+            )
 
         if st.button("Ask"):
             try:
@@ -34,17 +39,30 @@ if uploaded_file is not None:
                         model,
                         client
                         )
-                    st.session_state.messages.append(
-                        {"role": "user", "content": question_input})
-                    st.session_state.messages.append(
-                        {"role": "assistant", "content": chat_response})
+                    st.session_state[f"messages_{section_key}"].append(
+                        {
+                            "role": "user",
+                            "content": question_input
+                            }
+                            )
+                    st.session_state[f"messages_{section_key}"].append(
+                        {
+                            "role": "assistant",
+                            "content": chat_response
+                            }
+                            )
             except Exception as e:
-                st.session_state.messages.append(
-                    {"role": "assistant", "content": f"Error: {str(e)}"})
+                st.session_state[f"messages_{section_key}"].append(
+                    {
+                        "role": "assistant",
+                        "content": f"Error: {str(e)}"
+                        }
+                        )
                 st.error("Failed to process the question.")
 
-        for message in st.session_state.messages:
+        for message in st.session_state[f"messages_{section_key}"]:
             with st.chat_message(message["role"]):
                 st.write(message["content"])
+
     with col2:
         display_pdf(uploaded_file)
